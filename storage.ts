4af6f0c8d3db68cc24dd0efe33ececcd93262c24
kevinfiol <me@kevinfiol.com>
2024-03-25 00:41:07 -0400

    compress strings

--- storage.ts
+++ storage.ts
@@ -1,3 +1,5 @@
+import lz from 'lz';
+
 export interface Paste {
   paste: string;
   editCode?: string;
@@ -7,10 +9,18 @@ const KV = await Deno.openKv();
 
 export const storage = {
   async get(id: string) {
-    return await KV.get<Paste>([id]);
+    const result = await KV.get<Paste>([id]);
+
+    if (result.value !== null) {
+      result.value.paste = lz.decompress(result.value.paste);
+    }
+
+    return result;
   },
 
   async set(id: string, pasteAttrs: Paste) {
+    const compressed = lz.compress(pasteAttrs.paste) as Paste['paste'];
+    pasteAttrs.paste = compressed;
     return await KV.set([id], pasteAttrs);
   },
 
