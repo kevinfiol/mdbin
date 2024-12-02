120ab0eb45686fcd7840fa60ea1955dce3512e26
kevin <me+github@kevinfiol.com>
2024-12-02 12:34:19 -0500

    Move env var

--- env.ts
+++ env.ts
@@ -4,3 +4,5 @@ await load({ export: true });
 
 export const MODE = Deno.env.get('MODE') ?? 'prod';
 export const SERVER_PORT = Deno.env.get('SERVER_PORT') ?? 8000;
+export const DEMO_CLEAR_INTERVAL =
+  Number(Deno.env.get('DEMO_CLEAR_INTERVAL')) ?? 5;
